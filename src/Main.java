ab1945d0f60433ef6133e81fb06561c6aa2130ae
Sean Mikel Ra <smra14599@liceo.edu.ph>
2025-08-04 11:37:02 +0800

    main changes

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int totalNumber = 0;
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            int number = scanner.nextInt();
-            totalNumber += number;
-        }
-
-        System.out.println("Total Number: " + totalNumber);
-        scanner.close();
+        System.out.println("Main Branch");
     }
 }
