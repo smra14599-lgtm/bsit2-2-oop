7181af70a61cba8948d1c131fd74dda2eb52b784
Sean Mikel Ra <smra14599@liceo.edu.ph>
2025-08-18 11:51:37 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Main Branch");
+    public static void main  (String[] args) {
+        // 4. Creating instance and testing
+        GradeCalculator gc = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double average = gc.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = gc.getLetterGrade(average);
+
+
+        gc.displayResult(studentName, average);
+        gc.displayResult(studentName, average, letterGrade);
     }
 }
